7992e906c9565b10cf8739c1c8e220db0a0ba1e5
Desmond Ababio <dna35@cornell.edu>
2025-05-09 09:23:37 -0400

    Add checkpoint-2250 and updated files using Git LFS

--- backend/recipe.py
+++ backend/recipe.py
@@ -2,7 +2,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 
 # Load model and tokenizer once
-model_path = './checkpoint-675'
+model_path = './checkpoint-2250'
 
 tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
@@ -23,10 +23,10 @@ def generate_recipe(ingredients_list, max_length=300):
 
     output = model.generate(
         inputs.input_ids,
-        max_length=max_length,
+        # max_length=max_length,
         num_return_sequences=1,
         no_repeat_ngram_size=2,
-        temperature=0.7,
+        temperature=0.95,
         top_p=0.9,
         do_sample=True,
         pad_token_id=tokenizer.eos_token_id,
@@ -35,3 +35,7 @@ def generate_recipe(ingredients_list, max_length=300):
 
     generated_text = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
     return generated_text
+
+
+
+# print(generate_recipe(["tomato", "broccoli", "grape"]))
